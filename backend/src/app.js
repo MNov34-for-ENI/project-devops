d66822a08785fe7b7dc5dd713d1844f9e75666e9
Corentin Bédel <corentin.bedel2025@campus-eni.fr>
2026-08-25 10:45:40 +0200

    Tested the grafana+prometheus locally, should work

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,22 @@ const cors = require('cors');
 const taskRoutes = require('./routes/task.routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
+const { register, metricsMiddleware } = require('./config/metrics');
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(metricsMiddleware);
 
 // Routes
 app.use('/api/tasks', taskRoutes);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 module.exports = app;
